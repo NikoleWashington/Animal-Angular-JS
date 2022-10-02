7022d6189eba40a71c36b60c7e7e8ab12bb73b7f
nwashington <nikolee1541@gmail.com>
2022-10-02 15:30:01 -0400

    initial commit

---
+++ src/app/animal.ts
@@ -0,0 +1,8 @@
+// What is an animal? Create an interface
+export interface Animal{
+    name: string;
+    species: string;
+    sound: string;
+    description: string;
+}
+
---
+++ src/app/mock-animals.ts
@@ -0,0 +1,18 @@
+import {Animal} from './animal';
+// Creating an array of animals. Normally we would use a database
+// like Firebase but we are hardcoding animals here
+// Don't forget to import this in animals.components.ts
+export const ANIMALS: Animal[] = [
+    {name:"Dumbo", species:"Elephant", sound:"Trumpet", description:"Gray all over with huge ears" },
+    {name:"Peekachoo", species:"Sugar Glider", sound:"Crabbing", description:"Gray with black stripe down back"},
+    {name:"Smokey", species:"Cat", sound:"Meow", description:"Gray and black tabby" },
+    {name:"Arnold", species:"HedgeHog", sound:"Snuffling", description:"Quills all down their back" },
+    {name:"Raja", species:"Panther", sound:"Roar", description:"Black all over with piercing yellow eyes" },
+    {name:"Baloo", species:"Chimpanzee", sound:"hoot", description:"Gray all over with huge ears" },
+    {name:"Flipper", species:"Dolphin", sound:"Clicks", description:"Gray all over with narrow nose" },
+    {name:"Flower", species:"Meerkat", sound:"Squeal", description:"Brown with a sweet face" },
+    {name:"Rover", species:"Dog", sound:"Bark", description:"Brown brindle with huge jowels" },
+    {name:"Peep", species:"Parakeet", sound:"Whistling", description:"Gray all over with huge ears" }
+
+]
+    
